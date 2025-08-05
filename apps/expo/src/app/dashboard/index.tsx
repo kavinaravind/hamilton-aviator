fd31846df8e00e64dc64f6c01cd802fdfe5257bb
Kavin Aravind <kvnaravind@gmail.com>
2025-08-05 15:20:45 -0400

    added base dashboard page with safe area

--- apps/expo/src/app/dashboard/index.tsx
+++ apps/expo/src/app/dashboard/index.tsx
@@ -1,10 +1,16 @@
-import { Text } from "react-native";
-import { Link } from "expo-router";
+import { SafeAreaView, Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
 
 export default function DashboardPage() {
   return (
-    <Link href="/dashboard/post/4754b45c-5c81-4465-91f7-9553cc915747">
-      <Text>Post</Text>
-    </Link>
+    <SafeAreaView>
+      <Stack.Screen options={{ title: "Hamilton Dashboard" }} />
+      <View className="h-full w-full p-4">
+        <Text className="py-2 text-3xl font-bold text-primary">Post</Text>
+        <Link href="/dashboard/post/4754b45c-5c81-4465-91f7-9553cc915747">
+          <Text className="py-4 text-foreground">Post</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
