ad835adcb10f156b3ce696c2d51fcbeaead0b7e6
Kavin Aravind <kvnaravind@gmail.com>
2025-08-10 15:33:02 -0400

    added sample logbook data, integrated into seeder

--- packages/db/src/scripts/seed.ts
+++ packages/db/src/scripts/seed.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
 import path from "path";
+import type { PgTable } from "drizzle-orm/pg-core";
 import { sql } from "@vercel/postgres";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import { Aircraft } from "../lib/schema/aircraft";
+import { Logbook } from "../lib/schema/logbook";
 
 function initDb() {
   config();
@@ -20,29 +22,41 @@ function initDb() {
   });
 }
 
-async function main() {
+async function seedTable<T>(table: PgTable, file: string, label: string) {
   const db = initDb();
 
-  console.log("Resetting Aircraft table...");
-  await db.delete(Aircraft);
-  console.log("Table reset complete.");
+  console.log(`Resetting ${label} table...`);
+  await db.delete(table);
+  console.log(`Table reset complete.`);
 
-  console.log("Importing data from JSON file...");
-  const absPath = path.join(process.cwd(), "./src/scripts/data/aircraft.json");
+  const absPath = path.join(process.cwd(), file);
   if (!fs.existsSync(absPath)) {
     throw new Error(`JSON file not found: ${absPath}`);
   }
 
   console.log(`Reading data from: ${absPath}...`);
   const json = fs.readFileSync(absPath, "utf-8");
-  const data = JSON.parse(json) as (typeof Aircraft.$inferInsert)[];
+  const data = JSON.parse(json) as T[];
   if (!Array.isArray(data)) {
     throw new Error("JSON file must contain an array of objects");
   }
 
-  console.log("Inserting data into Aircraft table...");
-  await db.insert(Aircraft).values(data);
-  console.log(`Imported ${data.length} records into Aircraft table.`);
+  console.log(`Inserting data into ${label} table...`);
+  await db.insert(table).values(data);
+  console.log(`Imported ${data.length} records into ${label} table.`);
+}
+
+async function main() {
+  await seedTable<typeof Aircraft.$inferInsert>(
+    Aircraft,
+    "./src/scripts/data/aircraft.json",
+    "Aircraft",
+  );
+  await seedTable<typeof Logbook.$inferInsert>(
+    Logbook,
+    "./src/scripts/data/logbook.json",
+    "Logbook",
+  );
 }
 
 main()
